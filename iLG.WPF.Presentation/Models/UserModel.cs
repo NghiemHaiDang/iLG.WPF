62425501ce479df9eeb72455cb29d801cf1c75a3
dangNghiem <nghiemhaidang3009@gmail.com>
2024-06-03 16:47:36 +0700

    Update Resource

--- iLG.WPF.Presentation/Models/UserModel.cs
+++ iLG.WPF.Presentation/Models/UserModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace iLG.WPF.Presentation.Models
 {
-    internal class UserModel
+    public class UserModel : ObservableObject
     {
+        [Required]
+        [MaxLength(100)]
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                SetProperty(ref _email, value);
+            }
+        }
+        [Required]
+        [MaxLength(100)]
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                SetProperty(ref _password, value);
+            }
+        }
     }
 }
